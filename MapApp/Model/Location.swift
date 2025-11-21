fc704c42c8a14fd4ac5452244a36b9bdbda6f883
Samhir <samhirtamajon@gmail.com>
2025-11-21 14:27:20 -0500

    Set up MVVM structure and seed initial location data • Add MVVM folders (Models, Views, ViewModels, Services) to organize the project • Populate LocationsDataService with initial locations to enable the Location struct to function • Reference image assets from the data so UI can load images immediately

---
+++ MapApp/Model/Location.swift
@@ -0,0 +1,20 @@
+//
+//  Location.swift
+//  MapApp
+//
+//  Created by Samhir Tamajon Hernandez on 11/21/25.
+//
+
+import Foundation
+import MapKit
+
+struct Location {
+
+    let name: String
+    let cityName: String
+    let coordinates: CLLocationCoordinate2D
+    let description: String
+    let imageNames: [String]
+    let link: String
+    
+}
