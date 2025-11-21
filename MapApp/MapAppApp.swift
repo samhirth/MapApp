3c92cbcd52504de61b74edf573b12676256f2edd
Samhir <samhirtamajon@gmail.com>
2025-11-21 13:28:44 -0500

    Initial Commit

---
+++ MapApp/MapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapAppApp.swift
+//  MapApp
+//
+//  Created by Samhir Tamajon Hernandez on 11/21/25.
+//
+
+import SwiftUI
+
+@main
+struct MapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
