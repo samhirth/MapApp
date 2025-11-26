4941249627c194c0ce03b98e3b3b2ea09333d613
Samhir <samhirtamajon@gmail.com>
2025-11-25 20:38:32 -0500

    feat: Scaffold LocationPreviewView with Learn More and Next actions - Add base layout with image, title, and action buttons - Introduce Learn More button to show additional location details (wiring pending) - Add Next button to navigate through locations (wiring pending) - Organize code into imageSection, titleSection, and action buttons for readability - Initial visual styling; further UI polish to follow

---
+++ MapApp/View/LocationPreviewView.swift
@@ -0,0 +1,88 @@
+//
+//  LocationPreviewView.swift
+//  MapApp
+//
+//  Created by Samhir Tamajon Hernandez on 11/25/25.
+//
+
+import SwiftUI
+
+struct LocationPreviewView: View {
+    
+    let location: Location
+    
+    var body: some View {
+        HStack {
+            VStack(spacing: 16) {
+                imageSection
+                titleSection
+            }
+            VStack(spacing: 8) {
+                learnMoreButton
+                nextButton
+                
+                
+            }
+        }
+    }
+}
+
+#Preview {
+    LocationPreviewView(location: LocationsDataService.locations.first!)
+}
+
+
+
+
+
+extension LocationPreviewView {
+    
+    private var imageSection: some View {
+        ZStack {
+            if let imageName = location.imageNames.first {
+                Image(imageName)
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 100, height: 100)
+                    .cornerRadius(10)
+            }
+        }
+        .padding(6)
+        .background(Color.white)
+        .cornerRadius(10)
+    }
+    
+    private var titleSection: some View {
+        VStack(alignment: .leading, spacing: 4) {
+            Text(location.name)
+                .font(.title2)
+                .fontWeight(.bold)
+            
+            Text(location.cityName)
+                .font(.subheadline)
+        }
+    }
+    
+    private var learnMoreButton: some View {
+        Button {
+            
+        } label: {
+            Text("Learn More")
+                .font(.headline )
+                .frame(width: 125, height: 28)
+        }
+        .buttonStyle(.borderedProminent)
+    }
+    
+    private var nextButton: some View {
+        Button {
+            
+        } label: {
+            Text("Next")
+                .font(.headline )
+                .frame(width: 125, height: 28)
+        }
+        .buttonStyle(.bordered)
+    }
+    
+}
